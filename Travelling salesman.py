5c2863eaf3361063d4db810e6cd4b4113fadeaca
namename <mail@email.com>
2024-12-10 21:31:09 +0200

    Final commit probs

--- Travelling salesman.py
+++ Travelling salesman.py
@@ -18,7 +18,7 @@ def filter_duplicates(seq):
         seen.add(i)
     return list(seen)
         
-print("Enter number of nodes")
+print("Enter number of towns")
 num_nodes = int(input())
 perms_list = []
 # inner comprehension creates num_nodes number of zeroes, 
@@ -38,3 +38,24 @@ path_in_string = "".join(path_in_numbers)
 permutations(path_in_string, 0)
 perms_list_actual = filter_duplicates(perms_list)
 #print(perms_list_actual)
+
+temp_path_cost = 0
+minimum = 0
+chosen_string_path = ''
+
+for current_path in perms_list_actual:
+    temp_path_cost = 0
+    minimum = float('inf')
+    current_path = str(start_town) + current_path + str(start_town)
+    
+    for town in range(len(current_path) - 1):
+        t1 = int(current_path[town])
+        t2 = int(current_path[town + 1])
+        temp_path_cost += matrix[t1][t2]
+    
+    if minimum > temp_path_cost:
+        minimum = temp_path_cost
+        chosen_string_path = current_path
+
+print(f"The shortest path {chosen_string_path} is {minimum} long.")
+
