b1e29ee6e5ce8cdd1eba3aaa5e0e338a89c1fc88
namename <mail@email.com>
2024-12-11 13:31:36 +0200

    Streamlining design

--- Travelling salesman.py
+++ Travelling salesman.py
@@ -17,43 +17,52 @@ def filter_duplicates(seq):
     for i in seq:
         seen.add(i)
     return list(seen)
+
+def minimum_path_size(perms_list_filtered):
+    temp_path_cost = 0
+    minimum = 0
+    chosen_string_path = ''
+
+    for current_path in perms_list_filtered:
+        temp_path_cost = 0
+        minimum = float('inf')
+        current_path = str(start_town) + current_path + str(start_town)
+        # start/end town is added to the string
         
-print("Enter number of towns")
+        for index in range(len(current_path) - 1):
+            t1 = int(current_path[index])
+            t2 = int(current_path[index + 1])
+            temp_path_cost += matrix[t1][t2] # cost summation
+        
+        if temp_path_cost < minimum :
+            minimum = temp_path_cost
+            chosen_string_path = current_path
+    
+    return chosen_string_path, minimum
+
+
+print("Enter the number of towns: ", end='')
 num_nodes = int(input())
-perms_list = []
+print("Enter the starting town (zero-indexed): ", end='')
+start_town = int(input())
+
 # inner comprehension creates num_nodes number of zeroes, 
 # while the outer comprehension creates num_nodes number of lists.
 matrix = [[0 for _ in range(num_nodes)] for _ in range(num_nodes)]
-print("Enter start town zero-indexed")
-start_town = int(input())
+perms_list = []
+
+print("Enter the distances between all towns:")
 for i in range(num_nodes):
     for j in range(i+1, num_nodes):
-        print(f"Enter distance between town {i} and town {j}")
+        print(f"Towns {i} and {j}: ", end='')
         matrix[i][j] = int(input())
         matrix[j][i] = matrix[i][j]
+
 # assigns all nodes to path_in_numbers list except the start/end town node
 path_in_numbers = [str(i) for i in range(num_nodes) if i != start_town]
 path_in_string = "".join(path_in_numbers)
 permutations(path_in_string, 0)
 perms_list_filtered = filter_duplicates(perms_list)
 
-temp_path_cost = 0
-minimum = 0
-chosen_string_path = ''
-
-for current_path in perms_list_filtered:
-    temp_path_cost = 0
-    minimum = float('inf')
-    current_path = str(start_town) + current_path + str(start_town) # start/end town is added to the string
-    
-    for index in range(len(current_path) - 1):
-        t1 = int(current_path[index])
-        t2 = int(current_path[index + 1])
-        temp_path_cost += matrix[t1][t2] # cost summation
-    
-    if temp_path_cost < minimum :
-        minimum = temp_path_cost
-        chosen_string_path = current_path
-
-print(f"The shortest path {chosen_string_path} is {minimum} long.")
-
+(string_path, path_size) = minimum_path_size(perms_list_filtered)
+print(f"The shortest path {string_path} is {path_size} long.")
